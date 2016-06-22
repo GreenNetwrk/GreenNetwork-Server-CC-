d6ec656a25d3b6296452507af621c685e7d94e5f
GreenCommunications <alexander.m.rankin@gmail.com>
2016-06-22 15:58:51 -0400

    fixed encryption

--- greennet.php
+++ greennet.php
@@ -1,17 +1,30 @@
 <?php
+include 'aes.php';
+
 $action = $_GET["action"];
 
 if($action == "register")
 {
     $domain = $_GET["domain"];
+
     if(!file_exists("domains/".$domain))
     {
         date_default_timezone_set("ETC");
 
-        mkdir("domains/"..$domain);
+        mkdir("domains/".$domain);
 
-        $file = fopen("domains/".$domain."accountData.json", "r");
+        $file = fopen("domains/".$domain."/accountData.json", "w");
 
-        $toAppend = array("password" => $_GET["password"], "data" => "Date ".date("Y-m-d"), "time" => "unix-epoch-ETC".time());
+        $shaKey = hash("sha224", $_GET["password"]);
+
+        $raw = new AES($_GET["password"], $shaKey, 256);
+
+        $encryption = $raw->encrypt();
+
+        $toAppend = array("password" => $encryption, "data" => "Date-".date("Y-m-d"), "time-" => "unix-epoch-ETC-".time());
+
+        fwrite($file, json_encode($toAppend));
+
+        fclose($file);
     }
 }
