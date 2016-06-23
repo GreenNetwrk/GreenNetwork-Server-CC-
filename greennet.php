faf5e9cfca24ae53241f818bb37a1c3968cc78e9
GreenCommunications <alexander.m.rankin@gmail.com>
2016-06-23 17:41:54 -0400

    Added gen session w/ minor tweaks

--- greennet.php
+++ greennet.php
@@ -1,7 +1,20 @@
 <?php
 include 'aes.php';
 
+//error_reporting(0);
+
 $action = $_GET["action"];
+
+function encryptionToPassword($encryption, $password)
+{
+    $shaKey = hash("sha224", $password);
+
+    $aes = new AES($encryption, $shaKey, 256);
+
+    $password = $aes->decrypt();
+
+    return $password;
+}
 
 if($action == "register")
 {
@@ -25,7 +38,7 @@ if($action == "register")
 
         $encryption = $raw->encrypt();
 
-        $toAppend = array("password" => $encryption, "date" => date("Y-m-d"), "time-" => "unix-epoch-ETC-".time());
+        $toAppend = array("password" => $encryption, "date" => date("Y-m-d"), "time" => time());
 
         fwrite($file, json_encode($toAppend));
 
@@ -67,7 +80,7 @@ if($action == "register")
 
             $encryption = $raw->encrypt();
 
-            $toAppend = array("password" => $encryption, "date" => date("Y-m-d"), "time-" => "unix-epoch-ETC-".time());
+            $toAppend = array("password" => $encryption, "date" => date("Y-m-d"), "time" => time());
 
             $file = fopen("domains/".$domain."/subDomains"."/".$sub."/accountData.json", "w");
 
@@ -81,5 +94,83 @@ if($action == "register")
         }
     } else {
         echo "non existing domain or existing subdomain";
+    }
+} else if($action == "generateNewSession")
+{
+    $password = $_GET["password"];
+
+    $domain = $_GET["domain"];
+
+    $subDomain = $_GET["subDomain"]; //--Not requred--//
+    
+    if(file_exists("domains/".$domain))
+    {
+        if($subDomain != null)
+        {
+            if(file_exists("domains/".$domain."/subDomains"."/".$subDomain))
+            {
+                $file = fopen("domains/".$domain."/subDomains"."/".$subDomain."/accountData.json", "r");
+
+                $rawJSON = fread($file, filesize("domains/".$domain."/subDomains"."/".$subDomain."/accountData.json"));
+
+                fclose($file);
+
+                $json = json_decode($rawJSON);
+
+                if(encryptionToPassword($json->{'password'}, $password); == $password)
+                {
+                    $file = fopen("domains/".$domain."/subDomains"."/".$subDomain."/sessionData.json", "w");
+
+                    $sessionID = rand(1000, 9999)."-".rand(1000, 9999);
+
+                    $toAppend = array("currentSession" => $sessionID, "date" => date("Y-m-d"), "time" => time());
+
+                    fwrite($file, json_encode($toAppend));
+
+                    fclose($file);
+
+                    echo "true";
+                } else
+                {
+                    echo "invalid password";
+                }
+            } else
+            {
+                echo "no such subdomain";
+            }
+        } else
+        {
+            if(file_exists("domains/".$domain))
+            {
+                $file = fopen("domains/".$domain."/accountData.json", "r");
+
+                $rawJSON = fread($file, filesize("domains/".$domain."/accountData.json"));
+
+                fclose($file);
+
+                $json = json_decode($rawJSON);
+
+                if(encryptionToPassword($json->{'password'}, $password); == $password)
+                {
+                    $file = fopen("domains/".$domain."/sessionData.json", "w");
+
+                    $sessionID = rand(1000, 9999)."-".rand(1000, 9999);
+
+                    $toAppend = array("currentSession" => $sessionID, "date" => date("Y-m-d"), "time" => time());
+
+                    fwrite($file, json_encode($toAppend));
+
+                    fclose($file);
+
+                    echo "true";
+                } else
+                {
+                    echo "invalid password";
+                }
+            } else
+            {
+                echo "no such domain";
+            } 
+        }
     }
 }
